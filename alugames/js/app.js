116a24fd76f00fea84b45b208c1c3dce81518a5e
netospadoni <netospadoni@gmail.com>
2024-07-20 15:15:23 -0400

    alugames final

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -6,12 +6,23 @@ function alterarStatus(id) {
 
     if (imagem.classList.contains('dashboard__item__img--rented')
     || botao.classList.contains('dashboard__item__button--return')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+        if (confirm(`Você tem certeza que deseja devolver ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            alert(`Você devolveu ${nomeJogo.textContent}!`)
+        } else {
+            alert(`Você desistiu de devolver ${nomeJogo.textContent}! Aproveite o jogo!`)
+        }
+
     } else {
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver';
+        if (confirm(`Você tem certeza que deseja alugar ${nomeJogo.textContent}?`)) {
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.classList.add('dashboard__item__button--return');
+            botao.textContent = 'Devolver';
+            alert(`Você alugou ${nomeJogo.textContent}! Aproveite o jogo!`)
+        } else {
+            alert(`Você desistiu de alugar ${nomeJogo.textContent}!`)
+        }
     }
 }
